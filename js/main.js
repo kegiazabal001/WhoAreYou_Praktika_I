27b44dcdbcd3d40a7b1c198fae88b455a10946f2
Kimetz Egiazabal <kegiazabal001@ikasle.ehu.eus>
2022-11-09 23:57:55 +0100

    main

---
+++ js/main.js
@@ -0,0 +1,56 @@
+import { folder, leftArrow } from "./fragments.js";
+import { fetchJSON } from "./loaders.js";
+
+function differenceInDays(date1) {
+    // YOUR CODE HERE
+}
+
+let difference_In_Days = differenceInDays(new Date("08-18-2022"));
+
+window.onload = function () {
+  document.getElementById(
+    "gamenumber"
+  ).innerText = difference_In_Days.toString();
+  document.getElementById("back-icon").innerHTML = folder + leftArrow;
+};
+
+let game = {
+  guesses: [],
+  solution: {},
+  players: [],
+  leagues: []
+};
+
+function getSolution(players, solutionArray, difference_In_Days) {
+ 
+    // YOUR CODE HERE 
+}
+
+Promise.all([fetchJSON("fullplayers"), fetchJSON("solution")]).then(
+  (values) => {
+
+    let solution;
+    
+    [game.players, solution] = values;
+
+    game.solution = getSolution(game.players, solution, difference_In_Days);
+    
+    console.log(game.solution);
+
+    document.getElementById(
+      "mistery"
+    ).src = `https://playfootball.games/media/players/${
+      game.solution.id % 32
+    }/${game.solution.id}.png`;
+
+
+      // YOUR CODE HERE
+    let addRow = setupRows( /* THIS NEEDS A PARAMETER */ );
+    // get myInput object...
+      // when the user types a number an press the Enter key:
+        addRow( /* the ID of the player, where is it? */);
+    //  
+
+
+  }
+);
